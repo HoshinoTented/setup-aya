bb6aae539417d81e0c8a54d6c0d2936e7ef0b8c0
HoshinoTented <hoshinotented@qq.com>
2025-09-12 23:18:49 +0800

    init

---
+++ src/aya-setup.ts
@@ -0,0 +1,33 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as tc from '@actions/tool-cache'
+import * as io from '@actions/io'
+import * as path from 'path'
+
+const fileName = "cli-fatjar.jar"
+
+type Aya = {
+  ayaHome: string,
+  cliJar: string
+}
+
+export async function setup(token: string, home: string, version: string): Promise<Aya> {
+  const octokit = github.getOctokit(token)
+  const { data: release } = await octokit.rest.repos.getReleaseByTag({
+    owner: "aya-prover",
+    repo: "aya-dev",
+    tag: version
+  })
+
+  const ayaHome = path.join(home, ".aya")
+  await io.mkdirP(ayaHome)
+  
+  const ayaJar = await tc.downloadTool(release.assets_url + "/" + fileName, path.join(ayaHome, fileName))
+
+  core.addPath(ayaHome)
+
+  return {
+    ayaHome: ayaHome,
+    cliJar: ayaJar
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as os from 'os'
+import { setup } from './aya-setup.js'
+
+const cliJarName = "cli-fatjar.jar"
+
+/**
+ * The main function for the action.
+ *
+ * @returns Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  try {
+    const token = core.getInput('token')
+    
+    const home = os.homedir()
+    const { cliJar: clijar } = await setup(token, home,"nightly")
+
+    await exec.exec(clijar, [ "--version" ])
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
