14dcb9ddb4463dfea27272b261a742a4d2d85678
Hoshino Tented <hoshinotented@qq.com>
2025-09-20 15:23:59 +0000

    ...

--- dist/index.js
+++ dist/index.js
@@ -33844,7 +33844,7 @@ async function setup(token, version) {
     const downloaded = await toolCacheExports.downloadTool(assetsUrl);
     const ayaHome = await toolCacheExports.cacheFile(downloaded, cliFileName, toolName, version);
     coreExports.debug('Aya is setup at ' + ayaHome);
-    const refind = toolCacheExports.find(toolName, cliFileName, 'x64');
+    const refind = toolCacheExports.find(toolName, version);
     coreExports.debug('Aya is found at: ' + refind);
     coreExports.debug('Done setup Aya.');
     return new Aya(require$$1$5.join(ayaHome, cliFileName));
