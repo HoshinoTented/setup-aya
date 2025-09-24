2199133c5379180140a478ecec6272e8414d1e9a
Hoshino Tented <hoshinotented@qq.com>
2025-09-24 17:39:38 +0000

    initial

---
+++ src/aya-setup.ts
@@ -0,0 +1,81 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+import * as tc from '@actions/tool-cache'
+import path from 'path'
+
+const ayaProver = 'aya-prover'
+const ayaDev = 'aya-dev'
+const toolName = 'aya'
+const cliFileName = 'cli-fatjar.jar'
+
+class Aya {
+  cliJar: string
+
+  constructor(cliJar: string) {
+    this.cliJar = cliJar
+  }
+
+  async run(...args: string[]): Promise<number> {
+    return exec.exec('java', ['-jar', this.cliJar, ...args])
+  }
+
+  async execOutput(...args: string[]): Promise<exec.ExecOutput> {
+    return exec.getExecOutput('java', ['-jar', this.cliJar, ...args])
+  }
+}
+
+export async function setup(token: string, version: string): Promise<Aya> {
+  core.debug('Setting up Aya with version: ' + version)
+
+  const octokit = github.getOctokit(token)
+
+  // Improve error message when getReleaseByTag fails, but we need to deal with scope problem
+  const { data: release } = await octokit.rest.repos.getReleaseByTag({
+    owner: ayaProver,
+    repo: ayaDev,
+    tag: version
+  })
+
+  const { data: assets } = await octokit.rest.repos.listReleaseAssets({
+    owner: ayaProver,
+    repo: ayaDev,
+    release_id: release.id
+  })
+
+  const cliJarAsset = assets.find((asset) => asset.name == cliFileName)
+
+  if (cliJarAsset == undefined) {
+    throw new Error(
+      'Asset ' + cliFileName + ' in release ' + release.name + ' is found.'
+    )
+  }
+
+  const assetsUrl = cliJarAsset.browser_download_url
+
+  core.debug('Downloading ' + assetsUrl)
+  const downloaded = await tc.downloadTool(assetsUrl)
+
+  // Obtain aya version
+  // const tmpAya = new Aya(path.join(downloaded))
+  // const { exitCode: exitCode, stdout: realVersion } =
+  //   await tmpAya.execOutput('--version')
+
+  // if (exitCode != 0) {
+  //   throw new Error('Failed to get aya version')
+  // }
+
+  // we need to use the real version in case `version == 'nightly-build'`
+  const ayaHome = await tc.cacheFile(
+    downloaded,
+    cliFileName,
+    toolName,
+    // realVersion.substring(4).trim()
+    '0.0.0' // hard code, sorry
+  )
+
+  core.debug('Aya is setup at ' + ayaHome)
+
+  core.debug('Done setup Aya.')
+  return new Aya(path.join(ayaHome, cliFileName))
+}
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import * as core from '@actions/core'
+import tc from '@actions/tool-cache'
+
+const cliJarName = 'cli-fatjar.jar'
+
+/**
+ * Current directory is the root of aya-dev, the jar is produced
+ */
+export async function run(): Promise<void> {
+  try {
+    const ayaVersion = core.getInput('version')
+    // const { stdout: stdout } = await exec.getExecOutput('java', [
+    //   '-jar',
+    //   ayaJar,
+    //   '--version'
+    // ])
+    // const version = stdout.substring(4).trim()
+    const ayaHome = await tc.cacheFile(
+      cliJarName,
+      cliJarName,
+      'aya',
+      ayaVersion
+    )
+    core.info('Aya is installed into: ' + ayaHome)
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
